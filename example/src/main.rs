9ea83b8ef3f9567efc31947bdaa4950208db515e
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2023-02-22 21:28:13 +0100

    Added async features

--- example/src/main.rs
+++ example/src/main.rs
@@ -8,12 +8,19 @@ struct UserInformation {
 }
 
 fn main() {
-    // create a new user
+    let user = UserInformation {
+        name: "Cora Francis".to_string(),
+        age: 24,
+    };
+
+    UserInformation::create();
+    // user::create();
+    /*  // create a new user
     UserInformation::create();
     //find or create a new user
     UserInformation::find_or_create();
     // find user by primary key
     UserInformation::find_by_pk();
     // delete a model record
-    UserInformation::destroy();
+    UserInformation::destroy(); */
 }
