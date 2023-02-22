07aca48c4063f09fcc0c6fe1c5392a642cd08110
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2023-02-23 00:34:44 +0100

    update

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,25 +2,24 @@ use xorm::IntoModel;
 use xorm_macro::IntoModel;
 
 #[derive(IntoModel)]
+#[allow(unused_variables, dead_code)]
 struct UserInformation {
     name: String,
     age: u64,
 }
 
 fn main() {
-    let user = UserInformation {
+    /*    let user = UserInformation {
         name: "Cora Francis".to_string(),
         age: 24,
-    };
+    }; */
 
-    UserInformation::create();
-    // user::create();
-    /*  // create a new user
+    // create a new user
     UserInformation::create();
     //find or create a new user
     UserInformation::find_or_create();
     // find user by primary key
     UserInformation::find_by_pk();
     // delete a model record
-    UserInformation::destroy(); */
+    UserInformation::destroy();
 }
