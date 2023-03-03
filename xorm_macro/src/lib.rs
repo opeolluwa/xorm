e698037527417701cbe87a2ee017e605226b7fe7
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2023-03-03 21:14:27 +0100

    breaking chages but the macro  crate complies

--- xorm_macro/src/lib.rs
+++ xorm_macro/src/lib.rs
@@ -12,7 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[proc_macro_derive(IntoModel)]
+// log error to stdout
+// TODO: fix this
+// macro_rules! bail {( $err_msg:expr$(, $span:expr)? $(,)? ) => (
+//     {
+//         let mut _span = ::proc_macro::Span::call_site();
+//         $( _span = $span; )?
+//         return ::syn::Error::new(_span, $err_msg)
+//                    .to_compile_error()
+//                    .into()
+//         ;
+//     }
+// )}
+// use syn::spanned::Spanned;
+
+#[proc_macro_derive(IntoModel, attributes(table_name))]
 pub fn find_by_pk_macro_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
     impl_macro(&ast)
@@ -20,16 +34,40 @@ pub fn find_by_pk_macro_derive(input: proc_macro::TokenStream) -> proc_macro::To
 
 /// the macro implementation
 fn impl_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
-    /* get the name of the struct on which the trait is implemented
-     // # example
-     #[derive(IntoModel)]
-    struct UserInformation{
-    some fields here
-    }
-    The name filed below will return `UserInformation`
-    */
+    // get the name of the struct on which the trait is implemented and the table name
     let name = &ast.ident;
+    let table_name_attribute = &ast
+        .attrs
+        .iter()
+        .find(|attr| attr.path.is_ident("table_name"));
 
+    // if the table name is  not provide log error thru the bail macro
+    // bail if it doesn't exist
+    // TODO: implement proper error handling
+    if table_name_attribute.is_none() {
+        panic!(
+            "missing table name attribute,\nPlease add table name\ne.g. #[table_name = user_information]",
+            // &ast.ident.span()
+        )
+    }
+
+    // let table_name = table_name_attribute.unwrap().parse_meta();
+
+    let tbl_name = match table_name_attribute.unwrap().parse_meta() {
+        Ok(syn::Meta::NameValue(value)) => {
+            if let syn::Lit::Str(table_name) = value.lit {
+                table_name
+            } else {
+                todo!()
+                // println!("expected a float value, e.g. #[orbital_period = 1.0]");
+            }
+        }
+        Ok(syn::Meta::Path(_)) | Ok(syn::Meta::List(_)) => todo!(),
+        Err(_) => {
+            /* panic!("expected a NameValue style attribue, e.g. #[orbital_period = 1.0]",), */
+            todo!()
+        }
+    };
     // get the names of the fields available on the struct that implement the trait
     let fields_named = {
         let fields = if let syn::Data::Struct(syn::DataStruct { fields, .. }) = &ast.data {
@@ -84,6 +122,7 @@ fn impl_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
     // the actual implementation of the traits
     let gen = quote::quote! {
         impl IntoModel for #name {
+            const TABLE_NAME :String = #tbl_name;
             // find record by primary key
             fn find_by_pk() {
                 println!("{}", #find_by_pk_query);
@@ -102,3 +141,8 @@ fn impl_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
     };
     gen.into()
 }
+
+/*   Ok(/*bad*/ _) => panic!(
+    "expected a NameValue style attribue, e.g. #[orbital_period = 1.0]",
+    // bad.path().span()
+), */
